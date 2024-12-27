28fe0167f992beae599e7183ab07075e691aa919
apple <apple@MacBook-Air-3.local>
2024-12-27 15:55:15 +0530

    updated

--- cachematrix.R
+++ cachematrix.R
@@ -3,13 +3,41 @@
 
 ## Write a short comment describing this function
 
-makeCacheMatrix <- function(x = matrix()) {
-
+makeCacheMatrix <- function(matrix = matrix()) {
+       inv <- NULL
+       set <- function(new_matrix) {
+              matrix <<- new_matrix
+              inv <<- NULL
+       }
+       get <- function() {
+              matrix
+       }
+       setInverse <- function(inverse) {
+              inv <<- inverse
+       }
+       
+       getInverse <- function() {
+              inv
+       }
+       
+       list(set=set,get=get,
+            setInverse=setInverse,
+            getInverse=getInverse)
 }
 
 
 ## Write a short comment describing this function
 
-cacheSolve <- function(x, ...) {
+cacheSolve <- function(matrix, ...) {
         ## Return a matrix that is the inverse of 'x'
+       inv <- matrix$getInverse()
+       if(!is.null(inv))
+       {
+              message("Getting Cached Data")
+              return(inv)
+       }
+       new_data <- matrix$get()
+       inv <- solve(new_data)
+       matrix$setInverse(inv)
+       inv
 }
